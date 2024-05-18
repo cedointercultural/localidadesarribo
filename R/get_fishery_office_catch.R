ee376f1f8e5ba3063f4a5269b74d92d6046aacff
Hem Nalini Morzaria-Luna <hemnalini@cedo.org>
2024-05-17 23:18:21 -0700

    created sheet with fishery offices and corrected get_fishery_office

--- R/get_fishery_office_catch.R
+++ R/get_fishery_office_catch.R
@@ -12,11 +12,23 @@
 #' @examples
 get_fishery_office_catch <- function(catchdata){
 
+  googlesheets4::gs4_deauth()
+  
+  #CONTINUE HERE, ADD COL NAMES
+  fish.offices.conapesca <- googlesheets4::read_sheet("1FS2Rs9rL5OQH4n4PM5HmYJqQ0RGtWgWBoC1njpoHXRY") %>% 
+    keep_when(CVE_LOC!="NULL") %>% 
+    mutate(CVE_LOC = as.character(CVE_LOC)) %>% 
+    dplyr::pull(CVE_LOC)
 
-  fish.offices <- catchdata %>%
+  fish.offices.catch <- catchdata %>%
     dplyr::distinct(fishery_office, NOM_ENT) %>%
     dplyr::rename(NOM_LOC = fishery_office) %>% 
-    dplyr::left_join(inegigc, by=c("NOM_ENT","NOM_LOC")) 
+    dplyr::left_join(inegigc, by=c("NOM_ENT","NOM_LOC")) %>% 
+    dplyr::mutate(CVE_LOC = as.character(CVE_LOC)) %>% 
+    keep_when(CVE_LOC %in% fish.offices.conapesca)
+  
+  
+  #fish.offices.catch %>% keep_when(is.na(`Oficina de Pesca`)) %>% View()  
     
     
   
